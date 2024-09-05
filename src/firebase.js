25f6ed732816e4dd7c92943df2984f578c0b82be
Navdeep Gupta <133591405+navdeep111@users.noreply.github.com>
2024-09-05 16:50:23 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth  } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDS2N6cma7iDmbOMT9aNxWNllczS43Kwb0",
-  authDomain: "login-auth-20380.firebaseapp.com",
-  projectId: "login-auth-20380",
-  storageBucket: "login-auth-20380.appspot.com",
-  messagingSenderId: "832583747735",
-  appId: "1:832583747735:web:4595f734d78950ba7001d2",
-  measurementId: "G-JMHT606HHG"
+ apiKey: "AIzaSyBjIOzCwuiSR3nhGwFw8U_hC1qpI9Fypvs",
+  authDomain: "mob-auth-91176.firebaseapp.com",
+  projectId: "mob-auth-91176",
+  storageBucket: "mob-auth-91176.appspot.com",
+  messagingSenderId: "689910011619",
+  appId: "1:689910011619:web:93958b02e635a0e21b97d6",
+  measurementId: "G-XQX1QNT89G"
 };
 
 // Initialize Firebase
