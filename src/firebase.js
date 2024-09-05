caa9903a2eac258253acdbe0ed99e61d5b7d95d8
Navdeep Gupta <133591405+navdeep111@users.noreply.github.com>
2024-09-05 17:44:32 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getAuth  } from "firebase/auth";
 
 const firebaseConfig = {
- apiKey: "AIzaSyBjIOzCwuiSR3nhGwFw8U_hC1qpI9Fypvs",
-  authDomain: "mob-auth-91176.firebaseapp.com",
-  projectId: "mob-auth-91176",
-  storageBucket: "mob-auth-91176.appspot.com",
-  messagingSenderId: "689910011619",
-  appId: "1:689910011619:web:93958b02e635a0e21b97d6",
-  measurementId: "G-XQX1QNT89G"
+ // apiKey: "AIzaSyBjIOzCwuiSR3nhGwFw8U_hC1qpI9Fypvs",
+ //  authDomain: "mob-auth-91176.firebaseapp.com",
+ //  projectId: "mob-auth-91176",
+ //  storageBucket: "mob-auth-91176.appspot.com",
+ //  messagingSenderId: "689910011619",
+ //  appId: "1:689910011619:web:93958b02e635a0e21b97d6",
+ //  measurementId: "G-XQX1QNT89G"
+
+  apiKey: "AIzaSyDOKQ8rMP_OcLHH66FzT2DnTj1sTCpJSKg",
+  authDomain: "login-otp-e845c.firebaseapp.com",
+  projectId: "login-otp-e845c",
+  storageBucket: "login-otp-e845c.appspot.com",
+  messagingSenderId: "467853239640",
+  appId: "1:467853239640:web:682a41719bf4925efaf12d",
+  measurementId: "G-26BKMKWMLF"
 };
 
 // Initialize Firebase
